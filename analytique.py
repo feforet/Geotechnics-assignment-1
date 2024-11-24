60a2196d34ce1333d5f1c188441967bd30813b0a
lherphelin <louis.herphelin@student.uclouvain.be>
2024-11-24 11:07:00 +0100

    zz

--- analytique.py
+++ analytique.py
@@ -2,13 +2,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-d = 5.7 # m 
-prof= 18.98 # m
+d = 5.76 # m 
+prof= 20.08 # m
 V0 = 2903138.044 + 41814.63 * d + 60994.1552 * d**2
 M0 = 520732074.4 + (528676*d + (7.2349*10**5)*d**2)*1.35
 
 Eb = 210 * 10**9 # Pa acier 
-Eh = 1
+Eh = 8648116.172467
 t= min(0.00635 + d/100, 0.09)
 I = np.pi * (d**4 - (d-2*t)**4)/64   # Moment d'inertie de la poutre cylindrique
 lambdaa = (Eh/(4*Eb*I))**0.25
@@ -18,7 +18,7 @@ y = np.exp(-z*lambdaa)*((-V0/(2*(lambdaa**3)*Eb*I) - M0/(2*(lambdaa**2)*Eb*I)) *
 
 # Plot
 plt.plot(y, z)
-plt.xlabel('Déflexion (m)')
+plt.xlabel('Déplacement latéral (m)')
 plt.ylabel('Profondeur (m)')
-plt.title('Déflexion de la poutre cylindrique')
+plt.title('Déplacement latéral du pieu en fonction de la profondeur')
 plt.show()
