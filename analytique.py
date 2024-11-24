88dedecde65df61fb9840fd4274239314aaffb9e
lherphelin <louis.herphelin@student.uclouvain.be>
2024-11-24 12:28:08 +0100

    zz

--- analytique.py
+++ analytique.py
@@ -13,11 +13,11 @@ t= min(0.00635 + d/100, 0.09)
 I = np.pi * (d**4 - (d-2*t)**4)/64   # Moment d'inertie de la poutre cylindrique
 lambdaa = (Eh/(4*Eb*I))**0.25
 
-z = np.linspace(0, -prof, 1000)
+z = np.linspace( 0, prof, 1000)
 y = np.exp(-z*lambdaa)*((-V0/(2*(lambdaa**3)*Eb*I) - M0/(2*(lambdaa**2)*Eb*I)) * np.cos(lambdaa*z) + (M0/(2*(lambdaa**2)*Eb*I)) * np.sin(lambdaa*z))
 
 # Plot
-plt.plot(y, z)
+plt.plot(-y, -z)
 plt.xlabel('Déplacement latéral (m)')
 plt.ylabel('Profondeur (m)')
 plt.title('Déplacement latéral du pieu en fonction de la profondeur')
